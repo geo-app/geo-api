430a926081cd98ef775be0ea51445adb61760105
Quentin MENDEL <qtn@macbook-pro-de-qtn.home>
2022-05-22 15:26:47 +0200

    feat: crud done

--- server.js
+++ server.js
@@ -5,11 +5,24 @@ const server = express();
 server.use(express.json());
 server.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:500000}));
 
+
+// Set Headers
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET', 'OPTIONS');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers",
+  );
+  next();
+});
+
 // Initialize & Configure MongoDB
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://admin:admin@main.doxhq.mongodb.net/geolocalisation-api?retryWrites=true&w=majority').then(
   console.log('Database connected')
 ).catch(err => console.log(err));
+
 
 // Import Controllers
 const communeController = require("./routes/communeController");
